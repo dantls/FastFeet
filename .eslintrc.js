51b0951d2317ac58f92a55bae276c2dc6dac2ba0
dantls <dan_tls@hotmail.com>
2020-02-08 17:44:27 -0400

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb','prettier'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react', 'prettier'
+  ],
+  rules: {
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase":"off",
+    "no-unused-vars":["error", {"argsIgnorePattern" : "next"}],
+    "prettier/prettier":"error"
+  },
+};
